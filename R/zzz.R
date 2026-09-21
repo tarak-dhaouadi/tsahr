2ea72721f7abca808dd5e700e036152e4a1dd7c0
tarak-dhaouadi <dhaouaditarak2016@gmail.com>
2026-09-21 12:55:54 +0100

    TSA of HR
    Trial sequential analysis for meta-analyses of hazard ratios

--- R/zzz.R
+++
@@ -1,5 +0,0 @@
-# log_HR and Std_Error are column names of the `data` argument, referenced
-# via non-standard evaluation inside metafor::rma(yi = log_HR, sei = Std_Error,
-# data = data, ...) -- exactly as in stats::lm() formulas. This declaration
-# tells R CMD check these are not undefined global variables.
-utils::globalVariables(c("log_HR", "Std_Error"))
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(tsahr)
-
-test_check("tsahr")
